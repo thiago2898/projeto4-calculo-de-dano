8cd2038b7ac6653a8e00f0cb13043a9fbaa3be10
Thiago Botelho <155117055+thiago2898@users.noreply.github.com>
2024-08-24 10:35:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const pers1 = prompt('Defina o nome do seu personagem.')
+const pers2 = prompt('Defina o nome de outro personagem.')
+let attackPower = parseInt(prompt(`Quanto vale o poder de ataque de ${pers1}?`))
+let life = parseInt(prompt(`Quantos pontos de vida ${pers2} tem?`))
+let defensePower = parseInt(prompt(`Quantos pontos de defesa ${pers2} tem?`))
+let haveShield = confirm(`${pers2} tem um escudo?`)
+let difference = 0
+let damage = 0
+let damageInlife = 0
+
+if (attackPower > defensePower &&  haveShield === false) {
+    difference = attackPower - defensePower
+    damage = difference
+    damageInlife = damage - life
+    if (damageInlife < 0) {
+        damageInlife *= -1
+        alert(`${pers1} atacou. ${pers2} sofreu ${damage} de dano e não possui escudo. Sua vida agora é ${damageInlife}`) 
+    } else {
+        alert(`${pers1} atacou. ${pers2} sofreu ${damage} de dano e não possui escudo. Sua vida agora é ${damageInlife}`) 
+    }
+    
+} else if (attackPower > defensePower && haveShield === true) {
+    difference = (attackPower - defensePower) / 2
+    damage = difference
+    damageInlife = damage - life
+    if (damageInlife < 0) {
+        damageInlife *= -1
+        alert(`${pers1} atacou. ${pers2} sofreu ${damage} de dano e possui um escudo. Sua vida agora é ${damageInlife}`) 
+    } else {
+        alert(`${pers1} atacou. ${pers2} sofreu ${damage} de dano e possui um escudo. Sua vida agora é ${damageInlife}`) 
+    }
+} else if (attackPower <= defensePower) {
+    alert(`${pers1} atacou, mas não afetou ${pers2}. Nenhum dano causado!`)
+}
